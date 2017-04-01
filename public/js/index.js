9ff13325f25ed8745826f86a97dc7f3bb5f31d77
MBeckers93 <markbeckers11@hotmail.com>
2017-04-01 12:53:15 +0200

    Styling chat app

--- public/js/index.js
+++ public/js/index.js
@@ -13,7 +13,7 @@ socket.on('newMessage', function (message) {
 	var li = jQuery('<li></li>');
 	li.text(`${message.from}: ${message.text}`);
 
-	jQuery('#chatapp-messages').append(li);
+	jQuery('#messages').append(li);
 });
 
 socket.on('newLocationMessage', function(message) {
@@ -24,28 +24,32 @@ socket.on('newLocationMessage', function(message) {
 	a.attr('href', message.url);
 	li.append(a);
 
-	jQuery('#chatapp-messages').append(li);
+	jQuery('#messages').append(li);
 });
 
-jQuery('#chatapp-form').on('submit', function(e) {
+jQuery('#message-form').on('submit', function(e) {
 	e.preventDefault();
+
+	var messageTextbox = jQuery('[name=message]');
 
 	socket.emit('createMessage', {
 		from: 'User',
-		text: jQuery('[name=chatapp-sendMessage]').val()
+		text: messageTextbox.val()
 	}, function () {
-
+		messageTextbox.val('');
 	});
 });
 
-var locationButton = jQuery('#sendLocation');
-
+var locationButton = jQuery('#send-location');
 locationButton.on('click', function() {
 	if (!navigator.geolocation) {
 		return alert('Geolocation not supported by your browser.');
 	}
 
+	locationButton.attr('disabled', 'disabled').text('Sending location...');
+
 	navigator.geolocation.getCurrentPosition(function (position) {
+    	locationButton.removeAttr('disabled').text('Send location');
 		socket.emit('createLocationMessage', {
 			latitude: position.coords.latitude,
 			longitude: position.coords.longitude
